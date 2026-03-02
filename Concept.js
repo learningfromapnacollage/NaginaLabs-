45b29cf03f7648670a61b07e327923d88f09c578
learningfromapnacollage <ulhassanzia91@gmail.com>
2026-03-02 05:32:50 +0500

    Enhance Concept.js with practice examples
    Added comments and logic examples to demonstrate JavaScript concepts.

---
+++ Concept.js
@@ -0,0 +1,45 @@
+let learningJsSeries = {
+ name : "Nagina Hassan",
+age : 15,
+isStudent : true,
+isTeenAger : true,
+concepts : {
+day1 : "print",
+day2 : "create variable",
+day3 : "variable rules",
+day4 : "key words",
+day5 : "data Type",
+day6 : "comments",
+day7 : "object",
+day8 : "arithmetic operator",
+day9 : "assigned operator",
+day10 : "comperison operator",
+day11 : "logical operator",
+day12 : "if statement"
+},
+conceptsClear : true,
+anyConfusion : false
+};
+// updating practice 
+let comments = 5;
+comments += 5;
+comments -= 5;
+comments *= 2;
+comments /= 2;
+console.log(comments);
+//commparing practice
+comments = 5;
+let dataType = 10;
+console.log(comments == dataType);
+console.log(comments != dataType);
+// let use some logic 
+console.log(comments && dataType);
+// using new concept 
+if(comments == 5) {
+  practice = false;
+}
+if(comments == 10) {
+  prctice = true;
+}
+console.log(practice);
+console.log(learningJsSeries);
