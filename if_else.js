e592efb48701aab38527015e71d1e17b9ca5a675
learningfromapnacollage <ulhassanzia91@gmail.com>
2026-03-05 05:06:27 +0500

    Add if-else logic for drink selection based on rain
    Implement if-else statement to determine drink based on rain condition.

---
+++ if_else.js
@@ -0,0 +1,35 @@
+/* To declare variable we use
+the key word "let" and also use the
+semicolon at the end of statment. 
+It represent the variable end.*/
+let rain = false;
+/* Create the variable again but not
+assign it a value yet ,we will 
+assign the value later depending
+upon the condition.*/
+let drink ;
+/* It is define as if_else statment.
+in the parenthesis we write 
+condition.*/
+if(rain ===  true) {
+/* After telling the condition to 
+computer according to the logic,
+we should check the condition 
+to know which output is correct.*/
+drink = "hot coffee";
+  /* If the  condition is according 
+  to the given input data it will 
+  print  hot  coffee.*/
+} else {
+  /* if the condition is opposite
+  to the given input data it will 
+  print cold juice.*/
+drink = "cold juice";
+}
+/* To know the output we use the 
+basic printing syntax ,by writing 
+the varible which is going to be 
+processed .*/
+console.log(drink);
+/* It print the finding variable 
+according to the condition.*/
