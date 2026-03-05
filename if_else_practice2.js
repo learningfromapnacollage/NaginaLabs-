8e923ce4df74865e29a7dfd4363eb7142e8e00e6
learningfromapnacollage <ulhassanzia91@gmail.com>
2026-03-06 04:48:19 +0500

    Create if_else_practice2.js

---
+++ if_else_practice2.js
@@ -0,0 +1,25 @@
+/* Create a variable and assign it
+a data.*/
+let age = 20;
+/* Create the second variabl and 
+does not assign a value.*/
+let license ;
+/* Now use the If_else condition 
+to determine ,in which age you will
+create the license.*/
+if(age >= 18) {
+  /* This condition states that if 
+  age is equal or greater than the 
+  18 ,you can create license so,it
+  will print if condition .*/
+  license = "create";
+} else {
+  license = "can not create";
+}
+/* If the condition is not true it 
+will print false ,else condition 
+so , you cannot create the licence*/
+console.log(license);
+/* This syntax print either create 
+or can not create according to the 
+given condition .*/
