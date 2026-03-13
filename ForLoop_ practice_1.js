13f33f6c4a2bc16fb82e9c19009c50c08c5c2156
learningfromapnacollage <ulhassanzia91@gmail.com>
2026-03-13 09:31:31 +0500

    Create ForLoop_ practice_1.js

---
+++ ForLoop_ practice_1.js
@@ -0,0 +1,28 @@
+//Create varible
+let cake = 25;
+/* use for loop to execute code 
+again and again.
+"for" loop's name.
+"parenthesis" loop's syntax.
+in parentheses we right three
+conditions.
+1_
+initial. condion:
+it states from where the loop start.
+2_ 
+stoping. condition:
+it states how many time that piece 
+of code execute.
+3_
+updation. condtion:
+it states you can updates your 
+piece of code .
+"curly braces" loop's syntax where 
+we right. the actual code to be  
+execute again and again.
+after curly braces we come outside 
+from loop.*/
+for(let c = 1; c <= 10; c++) {
+  cake = cake + 1;
+}
+console.log("cake",cake);
