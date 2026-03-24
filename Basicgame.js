f5e5bdb760e65ac0efe218531d53ddd5865f7a2c
learningfromapnacollage <ulhassanzia91@gmail.com>
2026-03-24 06:00:04 +0500

    Add basic to-do list functionality with alerts
    The script initializes a to-do list and checks off tasks with alerts based on their completion status.

---
+++ Basicgame.js
@@ -0,0 +1,82 @@
+let toDoList = {
+  wakeUpEarly : true,
+  takeBath : true,
+  takeExercise : true,
+  takeBreakFast : true,
+  study : true,
+  playing : true,
+  prayer : true,
+  completeHomeTask : true
+};
+console.log(toDoList);
+/*Performing the task according to the
+  given conditions*/
+task;
+if(toDoList.wakeUpEarly == true){
+  task = true;
+  alert("congratulations you complete
+        your first task");
+} else {
+  task = false;
+  alert("complete this task" );
+}
+task;
+if(toDoList.takeBath == true) {
+  task = true;
+  alert("good job ");
+} else {
+  task = false;
+  alert("try again");
+}
+task;
+if(toDoList.takeExercise == true){
+  task = true;
+  alert("excellent");
+} else {
+  task = false;
+  alert("try again");
+} 
+task;
+if(toDoList.takeBreakFast == true) {
+  task = true;
+  alert("good job");
+} else {
+  task = false;
+  alert("try again");
+}
+task;
+if(toDoList.study == true) {
+  task = true;
+  alert("good job");
+} else {
+  task = false ;
+  alert("try again");
+}
+task;
+if(toDoList.playing == true) {
+  task = true;
+  alert("excellent");
+} else {
+  task = false;
+  alert("try again");
+}
+task;
+if(prayer == true) {
+  task = true;
+  alert("good job");
+} else {
+  task = false;
+  alert("try again");
+}
+task;
+if(toDoList.completeHomeTask == true){
+  task = true;
+  alert("good job");
+} else {
+  task = false;
+  alert("try again");
+}
+console.log(task);
+  
+        
+  
